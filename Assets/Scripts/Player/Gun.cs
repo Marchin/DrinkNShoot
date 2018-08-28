bece78fb36da8062881684b76309b12eaf27b3d8
Ignacio Mosconi <nacho7577@hotmail.com>
2018-08-28 11:08:47 -0300

    Shooting & Reloading - Alpha Version

---
+++ Assets/Scripts/Player/Gun.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+	[SerializeField] float damage;
+	[SerializeField] float fireRate;
+	[SerializeField] float range;
+	[SerializeField] float impactForce;
+	[SerializeField] int ammoLeft;
+	[SerializeField] int cylinderCapacity;
+	[SerializeField] AnimationClip shootAnimation;
+	[SerializeField] AnimationClip reloadAnimation;
+	float lastFireTime = 0;
+	int bulletsInCylinder = 0;
+	bool isReloading = false;
+
+	void Start()
+	{
+		bulletsInCylinder = cylinderCapacity; 
+	}
+
+	void Update()
+	{
+		if (Input.GetButtonDown("Fire1") && CanShoot())
+			Shoot();
+
+		if (Input.GetButton("Reload") && CanReload())
+			StartCoroutine(Reload());
+	}
+
+	void Shoot()
+	{
+		lastFireTime = Time.time;
+		bulletsInCylinder--;
+
+		RaycastHit hit;
+
+		if (Physics.Raycast(transform.position, transform.forward, out hit, range))
+		{
+			Rigidbody targetRigidbody = hit.transform.GetComponent<Rigidbody>();
+
+			if (targetRigidbody)
+				targetRigidbody.AddForce(-hit.normal * impactForce);
+		}
+	}
+
+	IEnumerator Reload()
+	{
+		isReloading = true;
+
+		for (int i = bulletsInCylinder; i < cylinderCapacity; i++)
+		{
+			yield return new WaitForSeconds(2);
+			bulletsInCylinder++;
+			ammoLeft--;
+		}
+
+		isReloading = false;
+	}
+
+	bool CanShoot()
+	{
+		return (!isReloading && Time.time - lastFireTime >= 1 / fireRate && bulletsInCylinder > 0);
+	}
+
+	bool CanReload()
+	{
+		return (!isReloading && Time.time - lastFireTime >= 1 / fireRate && ammoLeft > 0 && bulletsInCylinder < cylinderCapacity);
+	}
+
+	public float FireRate
+	{
+		get { return fireRate;}
+	}
+}
