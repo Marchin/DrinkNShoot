5c54dbf965d4410cf80bf22cd3483b6aa6baf268
Marchin <mmonterrosa94@gmail.com>
2018-08-29 12:32:04 -0300

    Setup Crow

---
+++ Drink n' Shoot/Assets/Scripts/Crow.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class Crow : MonoBehaviour {
+    [SerializeField] GameObject m_roof;
+    [SerializeField] float m_maxJumpInterval;
+    [SerializeField] float m_height;
+    [SerializeField] float m_maxMovementInterval;
+    [SerializeField] float m_distance;
+    Vector3 m_roofSize;
+    Vector3 m_targetPosition;
+    bool m_moving;
+    bool m_flying;
+
+    private void Awake() {
+        m_roofSize = Vector3.Scale(
+            m_roof.GetComponent<MeshRenderer>().bounds.size,
+            transform.localScale
+        );
+        if (m_targetPosition != transform.position) {
+            Vector3.Lerp(transform.position, m_targetPosition, Time.deltaTime);
+        }
+    }
+
+    void Move() {
+        if (!(m_moving && m_flying)) {
+            // m_targetPosition = 
+        }
+    }
+
+    void Flip() {
+        if (!(m_moving && m_flying)) {
+            // m_targetPosition = 
+        }
+
+    }
+
+    void Jump() {
+        if (!(m_moving && m_flying)) {
+            // m_targetPosition = 
+        }
+    }
+
+}
