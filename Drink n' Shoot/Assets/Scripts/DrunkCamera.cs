9c3f9bf7597157dcd45654f467bf0682e020dffb
Unknown <nicolaspiccitto@gmail.com>
2018-08-29 14:56:35 -0300

    Adjust camera shakness

--- Drink n' Shoot/Assets/Scripts/DrunkCamera.cs
+++ Drink n' Shoot/Assets/Scripts/DrunkCamera.cs
@@ -4,31 +4,27 @@ public class DrunkCamera : MonoBehaviour {
 	[SerializeField] Transform m_camera;
 	[SerializeField] float m_speed;
 	[SerializeField] float m_shakeness;
+	[SerializeField] float m_min = 0.3f;
 	const float m_HALF_PI = Mathf.PI / 2f;
-	const float MIN = 0.6f;
 	float m_shakenessMult;
 	float m_currRotation;
-	bool m_rotatingRight;
+    float m_prevRot; 
 
 	private void Awake() {
 		m_currRotation = m_camera.eulerAngles.z;
-		m_shakenessMult = Random.Range(MIN, 1f);
-		if (Mathf.Sin(f: Time.time * m_speed + m_HALF_PI) > 0f) {
-			m_rotatingRight = true;
-		} else {
-			m_rotatingRight = false;
-		}
+        m_prevRot = m_currRotation;
+		m_shakenessMult = Random.Range(m_min, 1f);
 	}
 
 	private void Update() {
 		Vector3 newRotation = m_camera.eulerAngles;
 		float oscillation = Mathf.Sin(f: Time.time * m_speed + m_HALF_PI);
-		if (((oscillation < 0f) && m_rotatingRight) ||
-			(oscillation > 0f) && !m_rotatingRight) {
-			m_shakenessMult = Random.Range(MIN, 1f);
-		}
 		m_currRotation += oscillation * m_shakeness * m_shakenessMult;
-		newRotation.z = m_currRotation;
+        if (Mathf.Sign(m_prevRot) != Mathf.Sign(m_currRotation)) {
+            m_shakenessMult = Random.Range(m_min, 1f);
+        }
+        newRotation.z = m_currRotation;
+        m_prevRot = m_currRotation;
 		m_camera.eulerAngles = newRotation;
 	}
 }
