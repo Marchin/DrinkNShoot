ffe53c4c036d4b67cb033d6c51a2daf595a809ec
Unknown <mmonterrosa94@gmail.com>
2018-08-30 16:45:29 -0300

    Add roof detection

--- Drink n' Shoot/Assets/Scripts/Crow.cs
+++ Drink n' Shoot/Assets/Scripts/Crow.cs
@@ -6,25 +6,52 @@ public class Crow : MonoBehaviour {
     [SerializeField] float m_height;
     [SerializeField] float m_maxMovementInterval;
     [SerializeField] float m_distance;
+    Animator m_anim;
     Vector3 m_roofSize;
     Vector3 m_targetPosition;
+    float m_distToFront;
     bool m_moving;
     bool m_flying;
 
     private void Awake() {
-        m_roofSize = Vector3.Scale(
-            m_roof.GetComponent<MeshRenderer>().bounds.size,
-            transform.localScale
-        );
-        if (m_targetPosition != transform.position) {
-            Vector3.Lerp(transform.position, m_targetPosition, Time.deltaTime);
+        // m_roofSize = Vector3.Scale(
+        //     m_roof.GetComponent<MeshRenderer>().bounds.size,
+        //     transform.localScale
+        // );
+        // if (m_targetPosition != transform.position) {
+        //     Vector3.Lerp(transform.position, m_targetPosition, Time.deltaTime);
+        // }
+        m_anim = GetComponent<Animator>();
+        m_distToFront = GetComponent<BoxCollider>().bounds.extents.z;
+        m_moving = false;
+        m_flying = false;
+        m_targetPosition = transform.position;
+    }
+
+    private void Update() {
+        Move();
+        if (transform.position != m_targetPosition) {
+            transform.position = Vector3.Lerp(transform.position, m_targetPosition, Time.deltaTime * 2f);
+        } else {
+            ResetState();
         }
     }
 
     void Move() {
         if (!(m_moving && m_flying)) {
-            // m_targetPosition = 
+            if (Physics.Raycast(transform.position + transform.forward * (m_distance + m_distToFront),
+                -transform.up,
+                2f
+            )){
+                m_targetPosition = transform.position + transform.forward * m_distance; 
+                m_moving = true;
+            }
         }
+    }
+
+    void ResetState(){
+        m_moving = false;
+        m_flying = false;
     }
 
     void Flip() {
