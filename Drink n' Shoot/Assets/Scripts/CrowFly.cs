0c4ec87b83556dd39256ea0252c8a5c1b885a90a
Marchin <mmonterrosa94@gmail.com>
2018-09-06 09:57:16 -0300

    Make Crow Fly (still needs polishing)
    Renamed Crow to CrowMovement

--- Drink n' Shoot/Assets/Scripts/CrowFly.cs
+++ Drink n' Shoot/Assets/Scripts/CrowFly.cs
@@ -6,47 +6,87 @@ public class CrowFly : MonoBehaviour {
 	[Tooltip("The zone in which the crow moves arround.")]
 	[SerializeField] MeshRenderer m_walkable;
 	[SerializeField] LayerMask m_crowsLayer;
+	[SerializeField] LayerMask m_sceneLayer;
 	[SerializeField] float m_height;
-	[SerializeField] float m_distance;
-	[SerializeField] readonly float MIN_INTERVAL;
+	[SerializeField] float MIN_INTERVAL;
+	[SerializeField] float m_speed;
+	[SerializeField] float m_rotationSpeed;
+	CrowMovement m_crowMovement;
 	Vector3 m_targetPosition;
 	Quaternion m_targetRotation;
 	float m_interval;
 	bool m_flying;
+	float m_accu;
+	float m_initHeight;
 
 	private void Awake() {
 		m_flying = false;
 		m_interval = MIN_INTERVAL;
+		m_accu = 0f;
 		m_targetPosition = transform.position;
 		m_targetRotation = transform.rotation;
+		m_crowMovement = GetComponent<CrowMovement>();
 	}
 
 	private void Update() {
 		m_interval -= Time.deltaTime;
-		if (m_interval <= 0f){
-			if (true /*is not moving and not flying*/) {
-				Fly();
+		if (m_interval <= 0f) {
+			if ( /*!m_crowMovement.IsMoving()*/ true && !m_flying) {
+				StartFly();
 			}
 		}
-	}
-
-	void Fly() {
-		if (GetLandingPosition(ref m_targetPosition)){
+		if (m_flying) {
+			Fly();
 		}
 	}
 
-	bool GetLandingPosition(ref Vector3 landingPosition){
+	void StartFly() {
+		if (GetLandingPosition(ref m_targetPosition)) {
+			m_targetRotation = Quaternion.LookRotation(
+				m_targetPosition - transform.position);
+			m_initHeight = transform.position.y;
+			m_flying = true;
+		}
+	}
+	//Hacer una cuadratica entre altura inicial, altura final, y altura de vuelo
+	void Fly() {
+		m_targetPosition.y = (Mathf.Sin(m_accu) * m_height) + m_initHeight;
+		m_accu += Time.deltaTime * m_speed;
+		transform.position = Vector3.Lerp(transform.position,
+			m_targetPosition, Time.deltaTime * m_speed);
+		transform.rotation = Quaternion.Lerp(transform.rotation,
+			m_targetRotation, Time.deltaTime * m_rotationSpeed);
+		RaycastHit hit;
+		bool touchedRoof = Physics.Raycast(transform.position,
+			Vector3.down, out hit, 0.5f, m_sceneLayer);
+		if (HasLanded() || touchedRoof) {
+			m_flying = false;
+			m_accu = 0f;
+			m_interval = MIN_INTERVAL;
+			transform.rotation = Quaternion.LookRotation(transform.forward, hit.normal);
+		}
+	}
+
+	bool GetLandingPosition(ref Vector3 landingPosition) {
 		Vector3 landing = m_walkable.transform.position;
-		landing.x += Random.Range(
-			-m_walkable.bounds.extents.x, m_walkable.bounds.extents.x);
-		landing.z += Random.Range
-		(-m_walkable.bounds.extents.z, m_walkable.bounds.extents.z);
-		bool isFreeToLand = 
-			!Physics.Raycast(landing + Vector3.up * m_height,
-				Vector3.down, m_height + m_walkable.bounds.max.y, m_crowsLayer); 
+		landing.x += Random.Range(-m_walkable.bounds.extents.x,
+			m_walkable.bounds.extents.x);
+		landing.z += Random.Range(-m_walkable.bounds.extents.z,
+			m_walkable.bounds.extents.z);
+		bool isFreeToLand = !Physics.Raycast(landing + Vector3.up * m_height,
+			Vector3.down, m_height + m_walkable.bounds.max.y, m_crowsLayer);
 		if (isFreeToLand) {
 			landingPosition = landing;
 		}
 		return isFreeToLand;
+	}
+
+	bool HasLanded() {
+		return ((transform.position == m_targetPosition) &&
+			(transform.rotation == m_targetRotation));
+	}
+
+	public bool IsFlying() {
+		return m_flying;
 	}
 }
---
+++ Drink n' Shoot/Assets/Scripts/CrowMovement.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+public class CrowMovement : MonoBehaviour {
+    [SerializeField] GameObject m_roof;
+    [SerializeField] float m_maxJumpInterval;
+    [SerializeField] float m_height;
+    [SerializeField] float m_maxMovementInterval;
+    [SerializeField] float m_distance;
+    CrowFly m_crowFly;
+    Vector3 m_roofSize;
+    Vector3 m_targetPosition;
+    Quaternion m_targetRotation;
+    float m_distToFront;
+    bool m_moving;
+    bool m_flipping;
+
+    private void Awake() {
+        // m_roofSize = Vector3.Scale(
+        //     m_roof.GetComponent<MeshRenderer>().bounds.size,
+        //     transform.localScale
+        // );
+        // if (m_targetPosition != transform.position) {
+        //     Vector3.Lerp(transform.position, m_targetPosition, Time.deltaTime);
+        // }
+        m_distToFront = GetComponent<BoxCollider>().bounds.extents.z;
+        m_crowFly = GetComponent<CrowFly>();
+        m_moving = false;
+        m_targetPosition = transform.position;
+    }
+
+    private void Update() {
+        Move();
+        if (transform.position != m_targetPosition) {
+            transform.position = Vector3.Lerp(
+                transform.position, m_targetPosition, Time.deltaTime * 2f);
+        }
+        if (transform.rotation != m_targetRotation) {
+            transform.rotation = Quaternion.RotateTowards(
+                transform.rotation, m_targetRotation, 2f * Time.deltaTime);
+        }
+        if (transform.position == m_targetPosition &&
+            transform.rotation == m_targetRotation) {
+
+            ResetState();
+        }
+    }
+
+    void Move() {
+        if (!(m_moving && m_crowFly.IsFlying() && m_flipping)) {
+            Vector3 targetOffset = transform.forward * (m_distance + m_distToFront);
+            if (Physics.Raycast(transform.position + targetOffset, -transform.up, 2f)) {
+
+                m_targetPosition = transform.position + transform.forward * m_distance;
+                m_moving = true;
+            } else {
+                Flip();
+            }
+        }
+    }
+
+    void ResetState() {
+        m_moving = false;
+        m_flipping = false;
+    }
+
+    void Flip() {
+        Vector3 flip = transform.eulerAngles;
+        flip.y += 180f;
+        m_targetRotation = Quaternion.Euler(flip);
+        m_flipping = true;
+    }
+
+    public bool IsMoving() {
+        return (m_moving || m_flipping);
+    }
+}
