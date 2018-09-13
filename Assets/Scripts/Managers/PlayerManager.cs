c9e7905c88b54ae8496288e356cafd97f8af39b6
Unknown <mmonterrosa94@gmail.com>
2018-09-13 14:52:16 -0300

    Add PlayerManager

---
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerManager : MonoBehaviour {
+	uint m_crowCurrency;
+	public uint CrowCurrency {
+		get {
+			return m_crowCurrency;
+		}
+		set {
+			m_crowCurrency = value;
+		}
+	}
+	
+	private void Awake() {
+		DontDestroyOnLoad(gameObject);
+	}
+}
