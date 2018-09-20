a6d09985f19ec1bf5004c803d51ab2da480e7b23
Unknown <mmonterrosa94@gmail.com>
2018-09-20 18:00:25 -0300

    Add CrowTrigger
    Make prefab for CrowSpawner

---
+++ Assets/Scripts/Enemies/Crows/CrowTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class CrowTrigger : MonoBehaviour {
+	[SerializeField] Collider[] m_landingZones;
+
+	private void Awake() {
+	}
+
+	private void OnTriggerEnter(Collider other) {
+		CrowSpawner.Instance.gameObject.SetActive(true);
+		CrowSpawner.Instance.SetLandingZones(m_landingZones);
+	}
+
+	private void OnTriggerExit(Collider other) {
+		CrowSpawner.Instance.gameObject.SetActive(false);		
+	}
+}
