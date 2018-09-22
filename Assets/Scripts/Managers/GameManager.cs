a9f5dfbef6b6540a57f7046f34ff76a78d588e00
Ignacio Mosconi <nacho7577@hotmail.com>
2018-09-22 13:09:42 -0300

    Created a Game Manager to Control the Volume Level

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	static GameManager instance;
+	SettingsMenu.GfxSetting currentGfxSetting = SettingsMenu.GfxSetting.Wild;
+	float currentSfxVolume = 0.75f;
+
+	void Awake()
+	{
+		if (Instance == this)
+			DontDestroyOnLoad(gameObject);
+		else
+			Debug.Log("There already was a Game Manager; avoid duplicates.", gameObject);
+
+	}
+
+	public static GameManager Instance
+	{
+		get
+		{
+			if (!instance)
+			{
+				instance = FindObjectOfType<GameManager>();
+
+				if (!instance)
+				{
+					GameObject gameObj = new GameObject("Game Manager");
+					instance = gameObj.AddComponent<GameManager>();
+				}
+			}
+			
+			return instance;
+		}
+	}
+
+	public SettingsMenu.GfxSetting CurrentGfxSetting
+	{
+		get { return currentGfxSetting; }
+		set { currentGfxSetting = value; }
+	}
+
+	public float CurrentSfxVolume
+	{
+		get { return currentSfxVolume; }
+		set { currentSfxVolume = value; }
+	}
+}
