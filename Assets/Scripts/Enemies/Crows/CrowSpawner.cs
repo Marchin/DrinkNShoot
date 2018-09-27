b0a172596bb1b650138ee87940fe61af7fc6d049
Unknown <mmonterrosa94@gmail.com>
2018-09-27 16:50:07 -0300

    Adjust spawn points
    They spawn at random point at a determined distance

--- Assets/Scripts/Enemies/Crows/CrowSpawner.cs
+++ Assets/Scripts/Enemies/Crows/CrowSpawner.cs
@@ -5,10 +5,15 @@ using UnityEngine;
 [RequireComponent(typeof(ObjectPool))]
 public class CrowSpawner : MonoBehaviour {
 	[SerializeField] float m_spawnInterval = 2f;
+	[SerializeField] float m_spawnDistance;
+	[SerializeField] float m_spawnHeight = 50f;
+	Transform m_player;
 	Collider[] m_landingZones;
 	ObjectPool m_pool;
+	
 	float m_counter;
 	private void Awake() {
+		m_player = FindObjectOfType<CameraRotation>().transform;
 		m_pool = GetComponent<ObjectPool>();
 		m_counter = m_spawnInterval;
 	}
@@ -17,7 +22,10 @@ public class CrowSpawner : MonoBehaviour {
 		if (m_counter <= 0f) {
 			GameObject go;
 			if (m_pool.Request(out go)) {
-				go.transform.position = Vector3.up * 50f;
+				float angle = Random.Range(0f, 360f);
+				Vector3 offset = new Vector3(Mathf.Cos(angle), 0f, Mathf.Sin(angle)) * m_spawnDistance;
+				offset.y = m_spawnHeight;
+				go.transform.position = m_player.position + offset ;
 				Crow crow = go.GetComponent<Crow>();
 				crow.SetLandingZones(m_landingZones);
 				crow.Init();
