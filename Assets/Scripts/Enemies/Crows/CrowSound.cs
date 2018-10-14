4218636ec3a5e42a9c7f4920436867893ddcc26d
Ignacio Mosconi <nacho7577@hotmail.com>
2018-10-14 12:53:56 -0300

    Added Sound Effects for the Crows

---
+++ Assets/Scripts/Enemies/Crows/CrowSound.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class CrowSound : MonoBehaviour
+{
+	[SerializeField] AudioSource[] crowSounds;
+	[SerializeField] [Range(0, 100)]
+	float makeSoundProbability = 45f;
+	static bool crowMakingSound = false;
+
+	void PlayRandomCrowSound()
+	{
+		if (!crowMakingSound && Random.Range(0f, 100f) < makeSoundProbability)
+		{
+			crowMakingSound = true;
+			int soundIndex = Random.Range(0, crowSounds.Length);
+			crowSounds[soundIndex].Play();
+			Invoke("NotMakingSound", crowSounds[soundIndex].clip.length);
+		}
+	}
+
+	void NotMakingSound()
+	{
+		crowMakingSound = false;
+	}
+}
