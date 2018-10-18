169a9703cfbdf15b272676c5fa2a15ca8e560ed8
Unknown <mmonterrosa94@gmail.com>
2018-10-18 15:13:49 -0300

    Add various poop images

--- Assets/Scripts/Enemies/Crows/PoopImage.cs
+++ Assets/Scripts/Enemies/Crows/PoopImage.cs
@@ -5,42 +5,23 @@ public class PoopImage : MonoBehaviour {
     [SerializeField] float m_maxAlpha = 0.5f;
     [SerializeField] float m_duration;
     [SerializeField] float m_fadeDuration;
-    Image m_image;
+    FadePoop[] m_images;
     AudioSource m_soundFX;
-    float m_fadingRate;
-    bool m_fading;
+    public float MaxAlpha {get{return m_maxAlpha;}}
+    public float Duration {get{return m_duration;}}
+    public float FadeDuration {get{return m_fadeDuration;}}
 
     private void Awake() {
-        m_fading = false;
-        m_image = GetComponent<Image>();
+        m_images = GetComponentsInChildren<FadePoop>();
         m_soundFX = GetComponent<AudioSource>();
-        m_fadingRate = m_maxAlpha / m_fadeDuration;
-        SetAlpha(0f);
     }
 
     private void Update() {
-        if (m_fading) {
-            SetAlpha(m_image.color.a - (m_fadingRate * Time.deltaTime));
-            if (m_image.color.a <= 0f) {
-                m_fading = false;
-            }
-        }
-    }
-
-    void SetAlpha(float alpha) {
-        Color color = m_image.color;
-        color.a = alpha;
-        m_image.color = color;
     }
 
     [ContextMenu("Poop")]
     public void Poop() {
-        SetAlpha(m_maxAlpha);
         m_soundFX.Play();
-        Invoke("StartToFade", m_duration);
-    }
-
-    void StartToFade() {
-        m_fading = true;
+        m_images[Random.Range(0, m_images.Length)].Poop();
     }
 }
