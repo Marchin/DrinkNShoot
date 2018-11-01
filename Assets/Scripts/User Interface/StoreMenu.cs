c00607e683b8fdcf0c622c0018fe4b029c913b16
Unknown <nacho7577@hotmail.com>
2018-11-01 17:09:53 -0300

    Made the Player Able to Purchase the Winchester from the Store

--- Assets/Scripts/User Interface/StoreMenu.cs
+++ Assets/Scripts/User Interface/StoreMenu.cs
@@ -1,20 +1,37 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class StoreMenu : MonoBehaviour
 {
 	[SerializeField] TextMeshProUGUI currencyText;
+	[SerializeField] Button purchaseButton;
 
 	void Start()
 	{
+		GameManager.Instance.ShowCursor();
 		currencyText.text = PlayerManager.Instance.Currency.ToString();
 	}
 
-	public void PurchaseWinchester()
+	public void PurchaseItem(string itemName)
 	{
-		// StoreManager.Instance.PurchaseGun();
+		
+		bool wasPurchased = false;
+
+		switch (itemName)
+		{
+			case "Winchester":
+				wasPurchased = StoreManager.Instance.PurchaseGun(itemName);
+				break;
+		}
+
+		if (wasPurchased)
+		{
+			currencyText.text = PlayerManager.Instance.Currency.ToString();
+			purchaseButton.interactable = false;
+		}
 	}
 
 	public void QuitStore()
