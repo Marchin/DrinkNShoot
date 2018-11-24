7f29b84ad96acd914c52d4f84f3b7dac914c611a
Ignacio Mosconi <nacho7577@hotmail.com>
2018-11-23 23:44:47 -0300

    Made the Player Rotate to Focus on the Current Stage

--- Assets/Scripts/Player/CameraRotation.cs
+++ Assets/Scripts/Player/CameraRotation.cs
@@ -1,4 +1,5 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class CameraRotation : MonoBehaviour 
 {
@@ -12,6 +13,7 @@ public class CameraRotation : MonoBehaviour
 	float minHorizontalAngle;
 	float maxHorizontalAngle;
 	bool horizontalRotationClamped = false;
+	bool horizontalRotationBeingClamped = false;
 	
 	void Awake()
 	{
@@ -40,18 +42,14 @@ public class CameraRotation : MonoBehaviour
 		if (horizontalRotationClamped)
 			horizontalAngle = Mathf.Clamp(horizontalAngle, minHorizontalAngle, maxHorizontalAngle);
 
-		transform.eulerAngles = new Vector3(transform.eulerAngles.x, horizontalAngle, transform.eulerAngles.z);
+		if (!horizontalRotationBeingClamped)
+			transform.eulerAngles = new Vector3(transform.eulerAngles.x, horizontalAngle, transform.eulerAngles.z);
 		fpsCamera.eulerAngles = new Vector3(verticalAngle, fpsCamera.eulerAngles.y, fpsCamera.eulerAngles.z);
 	}
 
 	void ChangeHorizontalClamping()
 	{
-		horizontalRotationClamped = true;
-		Vector3 targetDir = (LevelManager.Instance.CurrentStagePosition - LevelManager.Instance.CurrentSpawnPointPosition).normalized;
-		Quaternion targetCentralRotation = Quaternion.LookRotation(targetDir);
-
-		minHorizontalAngle = targetCentralRotation.eulerAngles.y - horizontalRange / 2f;
-		maxHorizontalAngle = targetCentralRotation.eulerAngles.y + horizontalRange / 2f;
+		StartCoroutine(FocusOnNextStage());
 	}
 
 	void UnclampHorizontalRotation()
@@ -59,5 +57,29 @@ public class CameraRotation : MonoBehaviour
 		horizontalRotationClamped = false;
 		minHorizontalAngle = 0f;
 		maxHorizontalAngle = 0f;
+	}
+
+	IEnumerator FocusOnNextStage()
+	{
+        horizontalRotationBeingClamped = true;
+        
+		float timer = 0f;
+		Vector3 targetDir = (LevelManager.Instance.CurrentStagePosition - LevelManager.Instance.CurrentSpawnPointPosition).normalized;
+        Quaternion targetCentralRotation = Quaternion.LookRotation(targetDir);
+		Quaternion fromRotation = transform.rotation;
+
+		while (transform.rotation.eulerAngles.y != targetCentralRotation.eulerAngles.y)
+		{
+            transform.rotation = Quaternion.Slerp(fromRotation, targetCentralRotation, timer); 
+			timer += Time.unscaledDeltaTime / 3f;
+			yield return null;
+		}
+
+		horizontalAngle = targetCentralRotation.eulerAngles.y;
+        minHorizontalAngle = targetCentralRotation.eulerAngles.y - horizontalRange / 2f;
+        maxHorizontalAngle = targetCentralRotation.eulerAngles.y + horizontalRange / 2f;
+        
+        horizontalRotationClamped = true;
+		horizontalRotationBeingClamped = false;
 	}
 }
