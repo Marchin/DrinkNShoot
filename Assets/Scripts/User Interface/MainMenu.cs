da7bcec38c0cbf00a1687ad493b95f4cd71bbeb8
Ignacio Mosconi <nacho7577@hotmail.com>
2018-11-28 10:03:58 -0300

    Added Background Music for the Main Menu

--- Assets/Scripts/User Interface/MainMenu.cs
+++ Assets/Scripts/User Interface/MainMenu.cs
@@ -5,12 +5,29 @@ using TMPro;
 public class MainMenu : MonoBehaviour
 {
 	[SerializeField] TextMeshProUGUI appVersionText;
+	[SerializeField] AudioSource themeSong;
+
+	[SerializeField] float musicStartDelay = 5f;
     
+	float musicTimer = 0f;
 
 	void Start()
 	{
 		GameManager.Instance.ShowCursor();
 		appVersionText.text = "Application Version: " + Application.version;
+	}
+
+	void Update()
+	{
+		if (!themeSong.isPlaying)
+		{
+			musicTimer += Time.deltaTime;
+			if (musicTimer >= musicStartDelay)
+			{
+				musicTimer = 0f;
+				themeSong.Play();
+			}
+		}
 	}
 
 	public void Play(int level)
