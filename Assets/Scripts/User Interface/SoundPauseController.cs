87da3a04e9a18523ae2a76a6a24ac9b7a9f7bce6
Marchin <mmonterrosa94@gmail.com>
2018-12-08 22:44:37 -0300

    Stop Horse and Wagon sound on pause

---
+++ Assets/Scripts/User Interface/SoundPauseController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class SoundPauseController : MonoBehaviour {
+    [SerializeField] AudioSource m_audioToPause;
+
+    private void Start() {
+        PauseMenu pauseMenu  = FindObjectOfType<PauseMenu>();
+        pauseMenu.OnPause.AddListener(Pause);
+        pauseMenu.OnResume.AddListener(Resume);
+    }
+
+    void Pause() {
+        m_audioToPause.Pause();
+    }
+
+    void Resume() {
+        m_audioToPause.Play();
+    }
+}
