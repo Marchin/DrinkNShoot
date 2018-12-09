ae8fb9cad5bb5c019c82805c7280843fbf112e72
Marchin <mmonterrosa94@gmail.com>
2018-12-08 22:11:35 -0300

    Add poop animation

--- Assets/Scripts/Enemies/Crows/FadePoop.cs
+++ Assets/Scripts/Enemies/Crows/FadePoop.cs
@@ -5,6 +5,7 @@ public class FadePoop : MonoBehaviour {
     float m_maxAlpha;
     float m_duration;
     float m_fadeDuration;
+    float m_fallSpeed;
 	Image m_image;
     float m_fadingRate;
     bool m_fading;
@@ -14,6 +15,7 @@ public class FadePoop : MonoBehaviour {
 		m_maxAlpha = poopImage.MaxAlpha;
 		m_duration = poopImage.Duration;
 		m_fadeDuration = poopImage.FadeDuration;
+        m_fallSpeed = poopImage.FallSpeed;
 		m_image = GetComponent<Image>();
         m_fadingRate = m_maxAlpha / m_fadeDuration;
         m_fading = false;
@@ -24,9 +26,12 @@ public class FadePoop : MonoBehaviour {
         if (m_fading) {
             SetAlpha(m_image.color.a - (m_fadingRate * Time.deltaTime));
             if (m_image.color.a <= 0f) {
-                m_fading = false;
+                enabled = false;
             }
         }
+        Vector2 pos = m_image.rectTransform.position;
+        pos.y -= m_fallSpeed * (m_image.color.a) * (m_image.color.a) * Time.deltaTime;
+        m_image.rectTransform.position = pos;
 	}
     void SetAlpha(float alpha) {
         Color color = m_image.color;
@@ -36,6 +41,9 @@ public class FadePoop : MonoBehaviour {
 
 	
     public void Poop() {
+        enabled = true;
+        m_image.rectTransform.position = new Vector2(
+            Random.Range(0.2f, 0.8f) * Screen.width, Random.Range(0.2f, 0.8f) * Screen.height);
         SetAlpha(m_maxAlpha);
         Invoke("StartToFade", m_duration);
     }
--- Assets/Scripts/Enemies/Crows/PoopImage.cs
+++ Assets/Scripts/Enemies/Crows/PoopImage.cs
@@ -6,24 +6,25 @@ public class PoopImage : MonoBehaviour {
     [SerializeField] float m_maxAlpha = 0.5f;
     [SerializeField] float m_duration;
     [SerializeField] float m_fadeDuration;
+    [SerializeField] float m_fallSpeed;
     FadePoop[] m_images;
     AudioSource m_soundFX;
-    
+
     UnityEvent onPoopAppear = new UnityEvent();
 
-    public UnityEvent OnPoopAppear {get{return onPoopAppear;}}
-    
-    public float MaxAlpha {get{return m_maxAlpha;}}
-    public float Duration {get{return m_duration;}}
-    public float FadeDuration {get{return m_fadeDuration;}}
+    public UnityEvent OnPoopAppear { get { return onPoopAppear; } }
+
+    public float MaxAlpha { get { return m_maxAlpha; } }
+    public float Duration { get { return m_duration; } }
+    public float FadeDuration { get { return m_fadeDuration; } }
+    public float FallSpeed { get { return m_fallSpeed; } }
 
     private void Awake() {
         m_images = GetComponentsInChildren<FadePoop>();
         m_soundFX = GetComponent<AudioSource>();
     }
 
-    private void Update() {
-    }
+    private void Update() { }
 
     [ContextMenu("Poop")]
     public void Poop() {
